53bf23dd3e61841b740985cafaf27d99c0f4cf49
91626 <91626@LAPTOP-U7GEUGF8>
2024-01-22 19:32:34 +0530

    Add project files.

---
+++ Interview Que/BasicInterview.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Que
+{
+    public class BasicInterview
+    {
+        public BasicInterview() { }
+        // in a string vowel is presented or not
+        public bool IsVowelPresent(string str)
+        {
+           foreach (char ch in str)
+           {
+                if (ch == 'A' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'E' || ch == 'a' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'e')
+                {
+                    return true;
+                }
+           }
+            return false;
+        }
+    }
+
+}
---
+++ Interview Que/Program.cs
@@ -0,0 +1,13 @@
+﻿using Interview_Que;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BasicInterview basicInterview = new BasicInterview();
+        //bool result = basicInterview.IsVowelPresent("Nishchhal");
+        //bool result2 = basicInterview.IsVowelPresent("XYZ");
+        //Console.WriteLine(result);
+        //Console.WriteLine(result2);
+    }
+} 
